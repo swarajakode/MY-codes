2dc278a2b6f4481e18cc26c2b132bb200e9977bd
swarajakode <44208504+swarajakode@users.noreply.github.com>
2018-10-17 02:04:27 +0530

    Create XORAGN.cpp

---
+++ XORAGN1/XORAGN.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    unsigned int arr[100001],T,i,n,o;
+    cin>>T;
+    while(T--)
+    {
+        cin>>n;
+        o=0;
+        for(i=0;i<n;i++)
+            {
+                cin>>arr[i];
+
+            }
+ for(i=0;i<n;i++)
+            {
+                o=o^arr[i];
+
+            }
+
+            o*=2;
+        cout<<o<<endl;
+    }
+
+    return 0;
+}
